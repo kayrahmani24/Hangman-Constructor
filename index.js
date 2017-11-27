6e8de0c12c4a4261bcfd5aee753e25ab83f7d40c
Kayvan Rahmani <Rahmani.kay1@gmail.com>
2017-11-27 16:08:42 -0600

    finished product

--- index.js
+++ index.js
@@ -1,64 +1,111 @@
 var inquirer = require("inquirer");
-var wordsArray = ["Dominos","Pizza Hut","Lou Malnatis","Giordanos","Peqouds","Pizanos"];
-var lettersGuessed = [];
-var wordInForm = "";
-var numBlanks = 0;
-var blanks = [];
-var chosenWord = "";
-var count = 9;
+var wordsArray = ["ginos east","coalfire","lou malnatis","giordanos","peqouds","pizanos"];
+	var lettersGuessed = [];
+	var wordInForm = "";
+	var numBlanks = 0;
+	var blanks = [];
+	var chosenWord = "";
+	var count = 10;
+	var blanksAndSuccesses = [];
+	var split = "";
 
-
+//constructor function
 function Hangman(){
 	this.word = function(){
-	
-	lettersGuessed = [];
-	wordInForm = "";
-	numBlanks = 0;
-	blanks = [];
-	chosenWord = "";
-	count = 9;
-
+	 //to reset global varibles
+	 lettersGuessed = [];
+	 wordInForm = "";
+	 numBlanks = 0;
+	 blanks = [];
+	 chosenWord = "";
+	 count = 10;
+	 blanksAndSuccesses = [];
+	 split = "";
+//chosing random word
 	chosenWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 	game.createBlanks(chosenWord);
-
 	
 };
 	
 this.createBlanks = function(word){
-
-	var split = word.split('');
-	console.log(split);
+//method to creat blanks for chosen word
+	split = word.split('');
+	//console.log(split);
 	numBlanks = split.length;
 	for( var i = 0; i < numBlanks; i++){
-	 blanks.push("_");	
+	 blanksAndSuccesses.push("_");	
 	}
-console.log(blanks);
+console.log(blanksAndSuccesses);
 game.runPrompt();
 	
 };
 
+this.complete = function(){
+	//method to check if user has guessed all the letters
+	 var checkWord = blanksAndSuccesses.join('');
+	 if (checkWord === chosenWord) {
+	 	console.log("Congratulations you win!!!");
+	 	game.word();
+	 }else {
+	 	game.runPrompt();
+	 }
+}
 
 this.checkLetter = function(letter){
-for ( var i = 0; i < numBlanks; i++){
-	
-};	
-if (chosenWord[i] === letter) {
-		numBlanks[i] = letter;
-		game.runPrompt();
-	}else{
-		console.log("Sorry try again!" + "\n Guesses Remaining: "+ count);
-		count--;
-		game.runPrompt();
+	//method to check if the letter guessed is in the chosen word code used from previous assignment
+	//console.log(split);
+var letterInWord = false;
 
+for (var i = 0; i < numBlanks; i++) {
+    if (chosenWord[i] === letter) {
+      letterInWord = true;
+    }
+  }
+
+  if (letterInWord) {
+
+   
+    for (var j = 0; j < numBlanks; j++) {
+
+    
+      if (chosenWord[j] === letter) {
+
+        blanksAndSuccesses[j] = letter;
+        numBlanks[i] = letter;
+         
+         	console.log("CORRECT!");
+          console.log(blanksAndSuccesses);   
+          lettersGuessed.push(letter);  
+          console.log("Letters you've already guessed: " + lettersGuessed);
+		 console.log("Guesses Remaining: "+ count);
+     		
+      }
+
+    }     	
+
+    game.complete();
+  }else{
+  	//if letter was wrong count decrements by one user is prompt again
+		lettersGuessed.push(letter);
+		
+		count--;
+		console.log("Sorry try again!");
+		
+		 console.log(blanksAndSuccesses);
+		 console.log("Letters you've already guessed: " + lettersGuessed);
+		 console.log("Guesses Remaining: "+ count);
+		 game.runPrompt();
 	}
 };
 this.runPrompt = function(){
+	//method to get the users input/letter they elect
 	if (count > 0) {
 inquirer.prompt([
       {
         name: "letter",
-        message: "Pick a letter!",
+        message: "Try to figure out the Chicago pizza joint I have in mind, pick a letter!",
        validate: function(value) {
+       	//validating if they are chosing a legitate leter of the alphabet
           if (isNaN(value) === true && value.length === 1) {
                    return true;
           }else {         
@@ -69,23 +116,25 @@ inquirer.prompt([
   }
     ])
  .then(function(answers) {
+//if letter is legimate we then run through check letter method aboce
+ 	console.log(answers.letter);
+ 	
       game.checkLetter(answers.letter);
-      
+    
 
   });
 }else {
-	console.log("Sorry no more guesses remaining!" + "\n The word was : " + chosenWord + "\n New game has begun maybe this time you'll get it!");
+	//if user has no more attempts we push this message and restart the game
+	console.log("Sorry no more guesses remaining!" + "\n The pizza joint was : " + chosenWord + "\n New game has begun maybe this time you'll get it!");
 	game.word();	
 }
 };
 
-
 };
-
+//Hangman object
 var game = new Hangman();
-
+//intializing the game
 game.word();
-
 
 
 
